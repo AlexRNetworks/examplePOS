ca1cd49962af922567ffca4c7c6c6aa7cb6cc556
AlexRNetworks <ctpardue@icloud.com>
2025-10-25 01:30:26 -0400

    Fix import statement formatting in index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css'; // We'll assume a basic CSS file is present
+import './index.css'; 
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
