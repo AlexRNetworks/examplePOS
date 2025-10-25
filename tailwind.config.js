1448579bb61dfdb88018caa64c915ecbf6bd5bbd
AlexRNetworks <ctpardue@icloud.com>
2025-10-25 01:50:57 -0400

    Add Tailwind CSS configuration file

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // Tells Tailwind which files to scan for utility classes
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./public/index.html",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
